956f82d30295736f0d68e58841f49062303548db
temty180447 <tgtv2547@gmail.com>
2024-07-04 17:23:31 +0700

    04

--- q2.ts
+++ q2.ts
@@ -1,9 +1,14 @@
 // define interface for Student object
-/* Your code here */
+interface Student {
+  name: string;
+  score: number;
+}
 
 // assign interface/type to the function definition properly
-function findTopNames(students) {
-  /* Your code here */
+function findTopNames(students: Student[]): string[] {
+  return students
+    .filter(student => student.score > 8)
+    .map(student => student.name);
 }
 
 // assign interface/type to the student1 object properly
