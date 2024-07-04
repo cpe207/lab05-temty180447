7d910c3d32a5717fac65f24c2377252e5be91840
temty180447 <tgtv2547@gmail.com>
2024-07-04 17:27:35 +0700

    05

--- q2.js
+++ q2.js
@@ -1,8 +1,8 @@
-// define interface for Student object
-/* Your code here */
 // assign interface/type to the function definition properly
 function findTopNames(students) {
-    /* Your code here */
+    return students
+        .filter(function (student) { return student.score > 8; })
+        .map(function (student) { return student.name; });
 }
 // assign interface/type to the student1 object properly
 var students1 = [
